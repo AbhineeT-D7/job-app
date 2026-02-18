f1e9434bbf1016916ee0a065c9edd58291d05f0d
AbhineeT-D7 <abhinetakg78965@gmail.com>
2026-02-19 05:01:58 +0530

    LoggingAspect.java

--- src/main/java/com/abhi/Job_APP2/aop/LoggingAspect.java
+++ src/main/java/com/abhi/Job_APP2/aop/LoggingAspect.java
@@ -12,22 +12,22 @@ public class LoggingAspect {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
-	@Before("execution(* com.subho.Job_APP2.Service.jobservice.*(..))")
+	@Before("execution(* com.abhi.Job_APP2.Service.jobservice.*(..))")
 	public void beforeMethodCall(JoinPoint jp) {
         LOGGER.info("Method called: {}", jp.getSignature().getName());
 	}
 
-	@After("execution(* com.subho.Job_APP2.Service.jobservice.*(..))")
+	@After("execution(* com.abhi.Job_APP2.Service.jobservice.*(..))")
 	public void afterMethodCall(JoinPoint jp) {
         LOGGER.info("Method executed: {}", jp.getSignature().getName());
 	}
 
-	@AfterThrowing("execution(* com.subho.Job_APP2.Service.jobservice.*(..))")
+	@AfterThrowing("execution(* com.abhi.Job_APP2.Service.jobservice.*(..))")
 	public void afterException(JoinPoint jp) {
         LOGGER.error("Exception occurred in method: {}", jp.getSignature().getName());
 	}
 
-	@AfterReturning("execution(* com.subho.Job_APP2.Service.jobservice.*(..))")
+	@AfterReturning("execution(* com.abhi.Job_APP2.Service.jobservice.*(..))")
 	public void afterSuccessfulReturn(JoinPoint jp) {
         LOGGER.info("Method executed successfully: {}", jp.getSignature().getName());
 	}
